b48cdcfae82e220911a2baa27f99f70180e18a8c
cloudPai <1565730756@qq.com>
2017-08-21 14:18:36 +0800

    基本功能实现 done

--- mysite3/mysite3/settings.py
+++ mysite3/mysite3/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 
 # #template
 # TEMPLATE_DIRS=(
-#     '/home/fnngj/djpy/mysite3/account/templates'
+#     '/Users/pailiu/github/django_register_teacher/mysite3/account/templates'
 # )
