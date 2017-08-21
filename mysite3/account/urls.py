8c3c6ba5f1aee7fcd238e096f9ac614bc02172c3
cloudPai <1565730756@qq.com>
2017-08-21 13:56:56 +0800

    register

---
+++ mysite3/account/urls.py
@@ -0,0 +1,13 @@
+
+from django.conf.urls import url
+
+from . import views
+# urlpatterns = patterns('',
+#                        url(r'^$', views.register, name='register'),
+#                        url(r'^register/$', views.register, name='register'),
+#                        )
+
+urlpatterns = [
+    url(r'^$', views.register, name='register'),
+    url(r'^register/$', views.register, name='register'),
+]
